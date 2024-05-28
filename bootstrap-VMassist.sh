d3fb43656a1b1eb4333415e9daa4d6a962b2d1de
pagienge <paul.gienger@microsoft.com>
2024-05-28 11:04:10 -0500

    typo

--- bootstrap-VMassist.sh
+++ bootstrap-VMassist.sh
@@ -6,10 +6,10 @@ echo "Creating $DEPLOYPATH"
 mkdir $DEPLOYPATH
 
 echo "downloading script(s)"
-wget --directory-prefix=$DEPLOYPATH https://github.com/pagienge/walinuxagenthealth/blob/main/VMaccess.sh
-wget --directory-prefix=$DEPLOYPATH https://github.com/pagienge/walinuxagenthealth/blob/main/VMaccess.py
+wget --directory-prefix=$DEPLOYPATH https://github.com/pagienge/walinuxagenthealth/blob/main/VMassist.sh
+wget --directory-prefix=$DEPLOYPATH https://github.com/pagienge/walinuxagenthealth/blob/main/VMassist.py
 
  cd $DEPLOYPATH
- echo "Script will not be auto-run, please run $DEPLOYPATH/VMaccess.sh once you have reviewed the content of the downloaded script(s)"
+ echo "Script will not be auto-run, please run $DEPLOYPATH/VMassist.sh once you have reviewed the content of the downloaded script(s)"
  exit
  
