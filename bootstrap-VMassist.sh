90c9973937c2d978af1d9e5d95d4a8b33fb2a266
pagienge <paul.gienger@microsoft.com>
2024-05-28 12:57:18 -0500

    more useful bootstrap

--- bootstrap-VMassist.sh
+++ bootstrap-VMassist.sh
@@ -6,8 +6,8 @@ echo "Creating $DEPLOYPATH"
 mkdir $DEPLOYPATH
 
 echo "downloading script(s)"
-wget --directory-prefix=$DEPLOYPATH https://github.com/pagienge/walinuxagenthealth/blob/main/VMassist.sh
-wget --directory-prefix=$DEPLOYPATH https://github.com/pagienge/walinuxagenthealth/blob/main/VMassist.py
+wget --directory-prefix=$DEPLOYPATH https://raw.githubusercontent.com/pagienge/walinuxagenthealth/main/VMassist.sh
+wget --directory-prefix=$DEPLOYPATH https://raw.githubusercontent.com/pagienge/walinuxagenthealth/main/VMassist.py
 
  cd $DEPLOYPATH
  echo "Script will not be auto-run, please run $DEPLOYPATH/VMassist.sh once you have reviewed the content of the downloaded script(s)"
