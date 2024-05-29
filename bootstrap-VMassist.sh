7bdd1117811773b111a703680b871d0a658620dd
pagienge <paul.gienger@microsoft.com>
2024-05-29 17:42:43 -0500

    even better wget

--- bootstrap-VMassist.sh
+++ bootstrap-VMassist.sh
@@ -6,11 +6,11 @@ echo "Creating $DLPATH"
 mkdir $DLPATH
 
 echo "downloading script(s)"
-wget --no-verbose --show-progress --directory-prefix=$DLPATH https://raw.githubusercontent.com/pagienge/walinuxagenthealth/main/VMassist.sh
+wget --no-verbose --directory-prefix=$DLPATH https://raw.githubusercontent.com/pagienge/walinuxagenthealth/main/VMassist.sh
 chmod +x $DLPATH/VMassist.sh
-wget --no-verbose --show-progress --directory-prefix=$DLPATH https://raw.githubusercontent.com/pagienge/walinuxagenthealth/main/VMassist.py
+wget --no-verbose --directory-prefix=$DLPATH https://raw.githubusercontent.com/pagienge/walinuxagenthealth/main/VMassist.py
 
  cd $DLPATH
  echo "Script will not be auto-run, please run $DLPATH/VMassist.sh once you have reviewed the content of the downloaded script(s)"
- return $?
+ 
  
